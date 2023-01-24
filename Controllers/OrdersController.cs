0b6f5a1f6fee4e72152260f8afb2690c1ad3e379
Bibovsky <48915487+Bibovsky@users.noreply.github.com>
2023-01-24 06:00:01 +0300

    Hm

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using VerstaTest.Models;
+
+namespace VerstaTest.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly VerstaTestContext _context;
+
+        public OrdersController(VerstaTestContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Orders
+        public async Task<IActionResult> Index()
+        {
+              return _context.Orders != null ? 
+                          View(await _context.Orders.ToListAsync()) :
+                          Problem("Entity set 'VerstaTestContext.Orders'  is null.");
+        }
+
+        // GET: Orders/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CityFrom,CityTo,AdressFrom,AdressTo,PackageWeight,PickUpDate")] Order order)
+        {
+            if (order.PickUpDate < (DateTime) SqlDateTime.MinValue) order.PickUpDate = (DateTime) SqlDateTime.MinValue;
+            if (order.PickUpDate > (DateTime) SqlDateTime.MaxValue) order.PickUpDate = (DateTime) SqlDateTime.MaxValue;
+
+
+            if (ModelState.IsValid)
+            {
+                order.Id = Guid.NewGuid();
+                
+
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,CityFrom,CityTo,AdressFrom,AdressTo,PackageWeight,PickUpDate")] Order order)
+        {
+            if (id != order.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Orders == null)
+            {
+                return Problem("Entity set 'VerstaTestContext.Orders'  is null.");
+            }
+            var order = await _context.Orders.FindAsync(id);
+            if (order != null)
+            {
+                _context.Orders.Remove(order);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderExists(Guid id)
+        {
+          return (_context.Orders?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace VerstaTest.Models;
+
+public partial class Order
+{
+    [DisplayName("Id")]
+    public Guid Id { get; set; }
+
+    
+    [DisplayName("Город отправителя")]
+    public string CityFrom { get; set; }
+    
+
+    [DisplayName("Город получателя")]
+    public string CityTo { get; set; }
+
+    [DisplayName("Адрес отправителя")]
+    public string AdressFrom { get; set; }
+
+
+    [DisplayName("Адрес получателя")]
+    public string AdressTo { get; set; }
+    
+
+    [DisplayName("Вес груза")] 
+    public double PackageWeight { get; set; }
+
+    
+    [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+    [DisplayName("Дата забора груза")]
+    public DateTime PickUpDate { get; set; }
+    
+
+}
